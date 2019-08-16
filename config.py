d10106e283c03bdc196eda5d53a592d7dc3224d4
Nathan Wiens <nathan.wiens@gmail.com>
2019-08-16 10:42:55 -0600

    Add files via upload

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 #Meraki MV Parameters
 # 
-MQTT_SERVER = "10.1.1.221"
-MERAKI_API_KEY = "XXXXXXXXXXX"
-NETWORK_ID = "YYYYYYYYYYYYY"
+MQTT_SERVER = "x.x.x.x"
+MERAKI_API_KEY = "AAA"
+NETWORK_ID = "BBB"
+microsoftapikey = "CCC"
+microsoftfaceapikey = "DDD"
+openalprsecret = "EEE"
 
 # Array of MV serial numbers
 COLLECT_CAMERAS_SERIAL_NUMBERS = ["*"]
@@ -17,16 +20,13 @@ MOTION_ALERT_ITERATE_COUNT = 4
 # Total people detections needed over MOTION_ALERT_ITERATE_COUNT to trigger notification
 MOTION_ALERT_TRIGGER_PEOPLE_COUNT = 0
 # Time between triggers, in seconds
-MOTION_ALERT_PAUSE_TIME = 15
+MOTION_ALERT_PAUSE_TIME = 5
 
 #WebEx Parameters
 #
 #WEBEXTEAMKEY is either you Personal Access Token or Bot API KEY, and starts with "Bearer "
 #https://developer.webex.com/docs/api/v1/people/list-people
-#Currently uses WebEx Bot: mvnotify@webex.bot
 WEBEXTEAMKEY = "Bearer ZmIyZTdkZTYtYzQzYi00NjQzLTk3MmUtZjA5ODZlYjc1NGFhNmQwNGVhZjMtMjBi_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
-
 #ROOM_ID is the room the script should post to. Must be writable by the API Key above
 #https://developer.webex.com/docs/api/v1/rooms/list-rooms
-#Add the bot to a room, then get the Room ID and paste below
-ROOM_ID = "ZZZZZZZZZZZZZZZZ"
+ROOM_ID = "ZZZ"
